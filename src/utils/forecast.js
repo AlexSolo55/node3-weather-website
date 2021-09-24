9ed6d382b6ced193cd342d7f8483e30124664653
alex solo <soloalekos55@gmail.com>
2021-09-24 22:39:03 +0300

    Add humidity in weather txt that show up

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longtitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + 
-                    ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+                    ' degress out. It feels like ' + body.current.feelslike + ' degress out, and the humidity is ' + body.current.humidity + '%.')
         }
     })
 }
