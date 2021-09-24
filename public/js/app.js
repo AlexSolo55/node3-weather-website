1df3e20071e5c881e7e319235097e75006f0f0b0
alex solo <soloalekos55@gmail.com>
2021-09-24 20:40:47 +0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+console.log('Client side javascript file is loaded')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.getElementById('message-1')
+const messageTwo = document.getElementById('message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?search=' + location).then((response) => {
+        response.json().then(({ error, location, forecast } = {}) => {
+            if (error) {
+                messageOne.textContent = error
+            } else {
+                messageOne.textContent = location
+                messageTwo.textContent = forecast
+            }
+        })
+    })
+})
